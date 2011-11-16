f57fb1079a0f22d3d3d24e6e5a4bafebac4f87a7
wc-duck <Fredrik.Kihlander@gmail.com>
2011-11-16 13:09:06 +0100

    fixed "klantskalle"-issue for win32-msvs-compile.

--- compat/msvs10/win32/link.bat
+++ compat/msvs10/win32/link.bat
@@ -1,8 +1,8 @@
 @echo off
+@set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin\;%VS100COMNTOOLS%..\..\Common7\IDE
+
 if "%ProgramFiles%" == "%ProgramFiles(x86)%" goto x64_PATH
 if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
-
-@set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin\;%VS100COMNTOOLS%..\..\Common7\IDE
 
 :x64_PATH
 @link.exe /libpath:"%VS100COMNTOOLS%..\..\VC\lib" /libpath:"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Lib" %*
