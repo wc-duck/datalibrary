4f5713ccd07311c7cb51d4bcffbf19e2fc730c13
Fredrik Kihlander <Fredrik.Kihlander@gmail.com>
2015-07-28 18:28:39 +0200

    Update build_bam.bat

--- compat/build_bam.bat
+++ compat/build_bam.bat
@@ -1,5 +1,5 @@
-c:\"Program Files (x86)"\"Microsoft Visual Studio 12.0"\VC\vcvarsall.bat
+call c:\"Program Files (x86)"\"Microsoft Visual Studio 12.0"\VC\vcvarsall.bat
 git clone https://github.com/matricks/bam.git
 cd bam
-make_win64_msvc.bat
+call make_win64_msvc.bat
 exit
