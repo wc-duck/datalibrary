094b05e0cf4d2011f6a62f7c93e53506462c4cf4
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2015-07-28 20:05:47 +0200

    build-bug search

--- compat/msvs10/winx64/link.bat
+++ compat/msvs10/winx64/link.bat
@@ -3,11 +3,13 @@ if "%ProgramFiles%" == "%ProgramFiles(x86)%" goto x64_PATH
 if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
 
 :x64_PATH
+echo "64 bit"
 set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin\amd64;%VS100COMNTOOLS%..\..\Common7\IDE
 link.exe /libpath:"%VS100COMNTOOLS%..\..\VC\lib\amd64" /libpath:"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Lib\x64" %*
 goto :eof
 
 :x86_PATH
+echo "32 bit"
 set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin\x86_amd64;%VS100COMNTOOLS%..\..\Common7\IDE
 link.exe /libpath:"%VS100COMNTOOLS%..\..\VC\lib\amd64" /libpath:"%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Lib\x64" %*
 goto :eof
