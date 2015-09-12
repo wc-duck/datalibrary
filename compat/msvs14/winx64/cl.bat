3b455960bda568fc9709201b1b96f35d22d27bdd
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2015-09-12 11:09:59 +0200

    msvs14 compile fix.

--- compat/msvs14/winx64/cl.bat
+++ compat/msvs14/winx64/cl.bat
@@ -4,10 +4,10 @@ if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
 
 :x64_PATH
 set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\amd64;%VS140COMNTOOLS%..\..\Common7\IDE
-cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Include" %*
+cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1\Include" %*
 goto :eof
 
 :x86_PATH
 set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\x86_amd64;%VS140COMNTOOLS%..\..\Common7\IDE
-cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Include" %*
+cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles%\Microsoft SDKs\Windows\v7.1\Include" %*
 goto :eof
