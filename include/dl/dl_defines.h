6eaab3b27be603eda98d2ff3bee0faa26af2d58b
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2015-10-30 11:19:58 +0100

    Removed DL_BIT from dl/dl_defines.h since it was almost unused.

--- include/dl/dl_defines.h
+++ include/dl/dl_defines.h
@@ -23,8 +23,7 @@
 	typedef uint32_t dl_typeid_t;
 #endif
 
-#define DL_BIT(_Bit)						( 1ULL << (_Bit) )
-#define DL_BITMASK(_Bits)                   ( DL_BIT(_Bits) - 1ULL )
+#define DL_BITMASK(_Bits)                   ( (1ULL << (_Bits)) - 1ULL )
 #define DL_BITRANGE(_MinBit,_MaxBit)		( ((1ULL << (_MaxBit)) | ((1ULL << (_MaxBit))-1ULL)) ^ ((1ULL << (_MinBit))-1ULL) )
 
 #define DL_ZERO_BITS(_Target, _Start, _Bits)         ( (_Target) & ~DL_BITRANGE(_Start, (_Start) + (_Bits) - 1ULL) )
