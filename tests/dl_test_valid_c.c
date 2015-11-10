dd404abcd1e3db942bf796d88b67e62d8530bbcd
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2015-11-10 09:47:44 +0100

    dl-headers is now valid c

--- tests/dl_test_valid_c.c
+++ tests/dl_test_valid_c.c
@@ -1,3 +1,10 @@
 #include "generated/unittest.h"
 #include "dl_test_included.h" // TODO: this should be included in unittest2.h someway.
 #include "generated/unittest2.h"
+
+#include <dl/dl.h>
+#include <dl/dl_convert.h>
+#include <dl/dl_reflect.h>
+#include <dl/dl_txt.h>
+#include <dl/dl_typelib.h>
+#include <dl/dl_util.h>
