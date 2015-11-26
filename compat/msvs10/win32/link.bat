1bcb721aaf3fe9a966c77de1f0d873fa005b809a
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2015-11-26 19:22:08 +0100

    Appveyor build fix?

--- compat/msvs10/win32/link.bat
+++ compat/msvs10/win32/link.bat
@@ -1,5 +1,5 @@
 @echo off
-@set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin\;%VS100COMNTOOLS%..\..\Common7\IDE
+@set PATH=%VS100COMNTOOLS%..\..\VC\bin\;%VS100COMNTOOLS%..\..\Common7\IDE;%PATH%
 
 if "%ProgramFiles%" == "%ProgramFiles(x86)%" goto x64_PATH
 if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
