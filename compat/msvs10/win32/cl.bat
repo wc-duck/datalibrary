614ef7c747cec9ec4f0dc72090771bb0111634b9
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2016-01-27 18:05:26 +0100

    simplified msvc-wrappers

--- compat/msvs10/win32/cl.bat
+++ compat/msvs10/win32/cl.bat
@@ -1,13 +1,4 @@
 @echo off
 set PATH=%PATH%;%VS100COMNTOOLS%..\..\VC\bin;%VS100COMNTOOLS%..\..\Common7\IDE
 
-if "%ProgramFiles%" == "%ProgramFiles(x86)%" goto x64_PATH
-if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
-
-:x64_PATH
-cl.exe /I "%VS100COMNTOOLS%..\..\VC\include" /I "%ProgramFiles(x86)%/Microsoft SDKs\Windows\v7.0A\Include" %*
-goto :eof
-
-:x86_PATH
 cl.exe /I "%VS100COMNTOOLS%..\..\VC\include" /I "%ProgramFiles%/Microsoft SDKs\Windows\v7.0A\Include" %*
-goto :eof
--- compat/msvs14/winx64/cl.bat
+++ compat/msvs14/winx64/cl.bat
@@ -1,13 +1,5 @@
 @echo off
-if "%ProgramFiles%" == "%ProgramFiles(x86)%" goto x64_PATH
-if "%ProgramFiles%" == "%ProgramW6432%" goto x86_PATH
+if "%ProgramFiles%" == "%ProgramFiles(x86)%" set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\amd64;%VS140COMNTOOLS%..\..\Common7\IDE
+if "%ProgramFiles%" == "%ProgramW6432%"      set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\x86_amd64;%VS140COMNTOOLS%..\..\Common7\IDE
 
-:x64_PATH
-set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\amd64;%VS140COMNTOOLS%..\..\Common7\IDE
-cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.1\Include" %*
-goto :eof
-
-:x86_PATH
-set PATH=%PATH%;%VS140COMNTOOLS%..\..\VC\bin\x86_amd64;%VS140COMNTOOLS%..\..\Common7\IDE
 cl.exe /I "%VS140COMNTOOLS%..\..\VC\include" /I "%ProgramFiles%\Microsoft SDKs\Windows\v7.1\Include" %*
-goto :eof
