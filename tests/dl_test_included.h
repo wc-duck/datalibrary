813a44d4cd06afcbf2520feed5ac75bf000fb87d
Victor Wåhlström <victor.wahlstrom@initiali.se>
2017-02-27 22:15:34 +0100

    C compliance fix for dl_test_valid_c.c

--- tests/dl_test_included.h
+++ tests/dl_test_included.h
@@ -4,7 +4,7 @@
 // this file should be included in the generated header from unittest2.tld!
 
 // this is specified as "extern" in unittest2.tld
-struct vec3_test { float x, y, z; };
+typedef struct vec3_test { float x, y, z; } vec3_test;
 
 #endif // DL_TEST_INCLUDED_H_INCLUDED
 
