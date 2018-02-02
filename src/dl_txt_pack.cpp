5c36f7750f62f014fb281b6bcf302e2b1cb7f90f
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2018-02-02 20:12:40 +0100

    argh!

--- src/dl_txt_pack.cpp
+++ src/dl_txt_pack.cpp
@@ -42,14 +42,14 @@ inline double dl_strtod(const char* str, char** endptr)
 				break;
 		}
 
-		if(_strnicmp("inf", str, 3) == 0)
-		{
-			*endptr = (char*)str + 3;
-			return std::numeric_limits<double>::infinity() * sign;
-		}
 		if(_strnicmp("infinity", str, 8) == 0)
 		{
 			*endptr = (char*)str + 8;
+			return std::numeric_limits<double>::infinity() * sign;
+		}
+		if(_strnicmp("inf", str, 3) == 0)
+		{
+			*endptr = (char*)str + 3;
 			return std::numeric_limits<double>::infinity() * sign;
 		}
 		if(_strnicmp("nan", str, 3) == 0)
