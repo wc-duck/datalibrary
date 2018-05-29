9ec2663e0118467bdca8b36df222e0206e4e7652
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2018-05-29 20:39:27 +0200

    fixed support for min/max values in enums.

--- tests/dl_tests_enum.cpp
+++ tests/dl_tests_enum.cpp
@@ -22,6 +22,23 @@ static_assert(DL_ALIGNOF(enum_uint16) == DL_ALIGNOF(uint16_t), "alignment of enu
 static_assert(DL_ALIGNOF(enum_uint32) == DL_ALIGNOF(uint32_t), "alignment of enum is incorrect");
 static_assert(DL_ALIGNOF(enum_uint64) == DL_ALIGNOF(uint64_t), "alignment of enum is incorrect");
 
+static_assert(int8_min  == INT8_MIN,    "should be equal!");
+static_assert(int8_max  == INT8_MAX,    "should be equal!");
+static_assert(int16_min == INT16_MIN,   "should be equal!");
+static_assert(int16_max == INT16_MAX,   "should be equal!");
+static_assert(int32_min == INT32_MIN,   "should be equal!");
+static_assert(int32_max == INT32_MAX,   "should be equal!");
+static_assert(int64_min == INT64_MIN,   "should be equal!");
+static_assert(int64_max == INT64_MAX,   "should be equal!");
+static_assert(uint8_min  == 0,          "should be equal!");
+static_assert(uint8_max  == UINT8_MAX,  "should be equal!");
+static_assert(uint16_min == 0,          "should be equal!");
+static_assert(uint16_max == UINT16_MAX, "should be equal!");
+static_assert(uint32_min == 0,          "should be equal!");
+static_assert(uint32_max == UINT32_MAX, "should be equal!");
+static_assert(uint64_min == 0,          "should be equal!");
+static_assert(uint64_max == UINT64_MAX, "should be equal!");
+
 TYPED_TEST(DLBase, sized_enums_simple)
 {
     sized_enums original;
@@ -70,4 +87,29 @@ TYPED_TEST(DLBase, sized_enums_simple_neg)
     EXPECT_EQ(uint16_1,  original.e_uint16);
     EXPECT_EQ(uint32_1,  original.e_uint32);
     EXPECT_EQ(uint64_1,  original.e_uint64);
+};
+
+TYPED_TEST(DLBase, sized_enums_simple_min)
+{
+    sized_enums original;
+    original.e_int8   = int8_min;
+	original.e_int16  = int16_min;
+	original.e_int32  = int32_min;
+	original.e_int64  = int64_min;
+	original.e_uint8  = uint8_min;
+	original.e_uint16 = uint16_min;
+	original.e_uint32 = uint32_min;
+	original.e_uint64 = uint64_min;
+
+    sized_enums loaded;
+    this->do_the_round_about( sized_enums::TYPE_ID, &original, &loaded, sizeof(loaded) );
+
+    EXPECT_EQ(int8_min,   original.e_int8);
+    EXPECT_EQ(int16_min,  original.e_int16);
+    EXPECT_EQ(int32_min,  original.e_int32);
+    EXPECT_EQ(int64_min,  original.e_int64);
+    EXPECT_EQ(uint8_min,  original.e_uint8);
+    EXPECT_EQ(uint16_min, original.e_uint16);
+    EXPECT_EQ(uint32_min, original.e_uint32);
+    EXPECT_EQ((uint64_t)uint64_min,  (uint64_t)original.e_uint64);
 };
