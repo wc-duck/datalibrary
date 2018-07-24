4d599ef7f91099ebc1a6b961c4b6f53455f22dc1
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2018-07-24 22:08:31 +0200

    temporarily reverted bam to use a known working revision for 'datalibrary' :(

--- compat/build_bam.bat
+++ compat/build_bam.bat
@@ -1,5 +1,6 @@
 call c:\"Program Files (x86)"\"Microsoft Visual Studio 12.0"\VC\vcvarsall.bat
 git clone https://github.com/matricks/bam.git
+git reset --hard c021a4c0d86dd0f042bf49046ee6bb51b88b8d96
 cd bam
 call make_win64_msvc.bat
 exit
