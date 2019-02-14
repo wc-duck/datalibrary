c9463a03ec53e5b26574c7dbe74f9a113bddee45
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2019-02-14 12:53:33 +0100

    Added support for default-values to be used in inline-arrays if all values isn't specified or 0 for pods.
    If you have an inline array of length 4 and you only write [1,2] the remaining slots will be set to:
    intXX/uintXX -> 0
    fp32/fp64 -> 0.0
    string -> nullptr
    pointer -> nullptr
    array -> [] ( i.e. nullptr + size 0 )
    
    in case of a struct it will use a completely default-initialized struct of that type, i.e. to use it
    like this it requires all members of the struct to have a "default" set.
    
    Note: there might be bugs in this functionallity since it's so new and untested... but what could possibly go wrong?
    
    Issue #105

--- tests/dl_tests_txt.cpp
+++ tests/dl_tests_txt.cpp
@@ -1276,3 +1276,94 @@ TEST_F( DLText, hex_uint64_invalid_range )
     dl_txt_test_expect_error<PodsDefaults>(Ctx, STRINGIFY( { PodsDefaults : { u64  :  0x10000000000000000 } } ), DL_ERROR_TXT_RANGE_ERROR);
     dl_txt_test_expect_error<PodsDefaults>(Ctx, STRINGIFY( { PodsDefaults : { u64  : -0x1                 } } ), DL_ERROR_TXT_RANGE_ERROR);
 }
+
+
+TEST_F( DLText, inline_array_fill_default)
+{
+	unsigned char unpack_buffer[1024];
+	test_inline_array_default_struct1* s = dl_txt_test_pack_text<test_inline_array_default_struct1>(this->Ctx,
+																									STRINGIFY( { "test_inline_array_default_struct1" : {} } ),
+																									unpack_buffer,
+																									sizeof(unpack_buffer));
+	EXPECT_EQ(s->arr[0].u, 1337u);
+	EXPECT_EQ(s->arr[1].u, 1337u);
+	EXPECT_EQ(s->arr[2].u, 1337u);
+}
+
+TEST_F( DLText, inline_array_fill_default_one_set)
+{
+	unsigned char unpack_buffer[1024];
+	test_inline_array_default_struct2* s = dl_txt_test_pack_text<test_inline_array_default_struct2>(this->Ctx,
+																									STRINGIFY( { "test_inline_array_default_struct2" : {} } ),
+																									unpack_buffer,
+																									sizeof(unpack_buffer));
+
+	EXPECT_EQ(s->arr[0].u, 7331u);
+	EXPECT_EQ(s->arr[1].u, 1337u);
+	EXPECT_EQ(s->arr[2].u, 1337u);
+}
+
+TEST_F( DLText, inline_array_auto_fill_u32)
+{
+	unsigned char unpack_buffer[1024];
+
+	{
+		WithInlineArray* s = dl_txt_test_pack_text<WithInlineArray>( this->Ctx,
+																	STRINGIFY( { "WithInlineArray" : { "Array" : [] } } ),
+																	unpack_buffer,
+																	sizeof(unpack_buffer) );
+
+		EXPECT_EQ(s->Array[0], 0u);
+		EXPECT_EQ(s->Array[1], 0u);
+		EXPECT_EQ(s->Array[2], 0u);
+	}
+
+	{
+		WithInlineArray* s = dl_txt_test_pack_text<WithInlineArray>( this->Ctx,
+																	STRINGIFY( { "WithInlineArray" : { "Array" : [] } } ),
+																	unpack_buffer,
+																	sizeof(unpack_buffer) );
+
+		EXPECT_EQ(s->Array[0], 0u);
+		EXPECT_EQ(s->Array[1], 0u);
+		EXPECT_EQ(s->Array[2], 0u);
+	}
+
+	{
+		WithInlineArray* s = dl_txt_test_pack_text<WithInlineArray>( this->Ctx,
+																	STRINGIFY( { "WithInlineArray" : { "Array" : [1] } } ),
+																	unpack_buffer,
+																	sizeof(unpack_buffer) );
+
+		EXPECT_EQ(s->Array[0], 1u);
+		EXPECT_EQ(s->Array[1], 0u);
+		EXPECT_EQ(s->Array[2], 0u);
+	}
+
+	{
+		WithInlineArray* s = dl_txt_test_pack_text<WithInlineArray>( this->Ctx,
+																	STRINGIFY( { "WithInlineArray" : { "Array" : [1,2] } } ),
+																	unpack_buffer,
+																	sizeof(unpack_buffer) );
+
+		EXPECT_EQ(s->Array[0], 1u);
+		EXPECT_EQ(s->Array[1], 2u);
+		EXPECT_EQ(s->Array[2], 0u);
+	}
+
+	{
+		WithInlineArray* s = dl_txt_test_pack_text<WithInlineArray>( this->Ctx,
+																	STRINGIFY( { "WithInlineArray" : { "Array" : [1,2,3] } } ),
+																	unpack_buffer,
+																	sizeof(unpack_buffer) );
+
+		EXPECT_EQ(s->Array[0], 1u);
+		EXPECT_EQ(s->Array[1], 2u);
+		EXPECT_EQ(s->Array[2], 3u);
+	}
+}
+
+TEST_F( DLText, empty_array_should_fail_if_no_default )
+{
+	dl_txt_test_expect_error<PodsDefaults>(Ctx, STRINGIFY( { "WithInlineArray" : {} } ), DL_ERROR_TXT_MISSING_MEMBER );	
+}
