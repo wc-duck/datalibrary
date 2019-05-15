2e279fb5f42a68de2997986b2235a8e873f6e7f8
Fredrik Kihlander <fredrik.kihlander@gmail.com>
2019-05-15 22:14:39 +0200

    had to revert to old version of bam on windows again =/

--- script/bootstrap.bat
+++ script/bootstrap.bat
@@ -1,6 +1,7 @@
 if not exist "local" mkdir local
 
 git clone https://github.com/matricks/bam.git local/bam
+git reset --hard c021a4c0d86dd0f042bf49046ee6bb51b88b8d96
 
 cd local/bam
 call make_win64_msvc.bat
